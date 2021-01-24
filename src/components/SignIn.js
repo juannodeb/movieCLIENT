ed6b1f868ac5a20b4d464fc1152c77343ce373fe
juannodeb <juannodeb@gmail.com>
2021-01-24 16:04:39 -0500

    feat: Pt.1 Re did the UI but the connection to the backend is still not fully implemented

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,15 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import auth from './auth/auth';
-import { useHistory } from 'react-router-dom';
-import { Grid, TextField, Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import { Zoom, TextField, Button } from '@material-ui/core';
 import { AccountCircle as User, VpnKey as Password } from '@material-ui/icons';
 
+const useStyles = makeStyles((theme) => ({
+  signIn: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    height: '90vh',
+  },
+
+  icons: {
+    color: '#5c6bc0',
+    fontSize: '3rem',
+    marginRight: '2rem',
+  },
+
+  field: {
+    marginBottom: '5rem',
+  },
+
+  mainButton: {
+    width: '18rem',
+    marginBottom: '2rem',
+  },
+
+  secondaryButton: {
+    width: '18rem',
+  }
+}));
+
 const SignIn = (props) => {
-  const SIGN_IN_ENDPOINT = process.env.REACT_APP_SIGN_IN_ENDPOINT;
+  // const SIGN_IN_ENDPOINT = process.env.REACT_APP_SIGN_IN_ENDPOINT;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-  const history = useHistory();
 
   const handleEmail = (e) => {
     setEmail(e.target.value);
@@ -20,55 +48,66 @@ const SignIn = (props) => {
   }
 
   const body = { email: email, password: password };
+  console.log('body: ', body);
 
-  async function sendCredentials(url = SIGN_IN_ENDPOINT, data = body) {
-    const response = await fetch(url, {
-      method: 'POST',
-      mode: 'cors',
-      cache: 'no-cache',
-      credentials: 'same-origin',
-      headers: { 'Content-Type': 'application/json'},
-      redirect: 'follow',
-      referrerPolicy: 'no-referrer',
-      body: JSON.stringify(data)
-    })
+  // async function sendCredentials(url = SIGN_IN_ENDPOINT, data = body) {
+  //   const response = await fetch(url, {
+  //     method: 'POST',
+  //     mode: 'cors',
+  //     cache: 'no-cache',
+  //     credentials: 'same-origin',
+  //     headers: { 'Content-Type': 'application/json'},
+  //     redirect: 'follow',
+  //     referrerPolicy: 'no-referrer',
+  //     body: JSON.stringify(data)
+  //   })
 
-    if (response.status === 200) {
-      history.push('/movies');
-    }
-  }
+  //   if (response.status === 200) {
+  //     history.push('/movies');
+  //   }
+  // }
+  const classes = useStyles();
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    setChecked(true);
+  },[])
 
   return (
-    <div>
-      <div style={{margin: '5rem 43%'}}>
-        <Grid container spacing={1} alignItems="flex-end">
-          <Grid item>
-            <User />
-          </Grid>
-          <Grid item>
-            <TextField id="email_field" label="Email" type="text" onChange={handleEmail}/>
-          </Grid>
-        </Grid>
-        <Grid container spacing={1} alignItems="flex-end">
-          <Grid item>
-            <Password />
-          </Grid>
-          <Grid item>
-            <TextField id="password_field" label="Password" type="password" onChange={handlePassword} />
-          </Grid>
-        </Grid>
-      </div>
-      <div style={{margin: '2rem 43%'}}>
-        {/* <Button variant="contained" color="primary" onClick={() => sendCredentials()}> */}
-        <Button variant="contained" color="primary" onClick={() => {
-          auth.login(() => {
-            props.history.push('/movies');
-          })
-        }}>
+    <Zoom in={checked}>
+      <div className={classes.signIn}>
+        <div className={classes.field}>
+          <User className={classes.icons} />
+          <TextField id="email_field" label="Email" type="text" onChange={handleEmail} />
+        </div>
+        <div className={classes.field}>
+          <Password className={classes.icons} />
+          <TextField id="password_field" label="Password" type="password" onChange={handlePassword} />
+        </div>
+        <Button
+          size="large"
+          variant="contained"
+          color="primary"
+          className={classes.mainButton}
+          onClick={() => {
+            auth.login(() => {
+              props.history.push('/movies');
+            })
+          }}
+        >
           Sign In
         </Button>
+        <Button
+          component={Link}
+          to={'/sign_up'}
+          size="large"
+          variant="outlined"
+          color="primary"
+          className={classes.secondaryButton}
+        >
+          Sign Up
+        </Button>
       </div>
-    </div>
+    </Zoom>
   )
 }
 
